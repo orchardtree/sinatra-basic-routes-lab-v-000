cf0357eca7cb37077308e769ec3acaa25e129d9f
Jesse Wenger <wengerj@gmail.com>
2021-04-02 21:20:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    
+  end
+   
+  get '/hometown' do
+    
+  end
+  
+  get '/favoritesong' do
+    
+  end
+  
 end
