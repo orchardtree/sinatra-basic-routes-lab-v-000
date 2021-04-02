2b8f91dca87a41411802729b16216b94257be5e0
Jesse Wenger <wengerj@gmail.com>
2021-04-02 21:23:07 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Jesse."
+  end
+   
+  get '/hometown' do
+    "My hometown is Chesapeake."
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Chicken Fried."
+  end
+  
 end
